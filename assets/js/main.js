2127220985bd6a29725a43f243adc24797b7640b
marianoperez.08 <marianoperez.c10@gmail.com>
2023-04-28 00:12:08 -0300

    changes responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,96 @@
-window.addEventListener("scroll", function() {
-    var navbar = document.querySelector(".navbar")
-    var scrollPos = window.scrollY
+let stockProductos = [
+    {
+        id: "remera-01",
+        nombre: "Remera Copa - 'Fenómeno'",
+        img: "remera-ronaldo.jpg",
+        categoria: {
+            nombre: "Remeras",
+            id: "remera"
+        },
+        precio: 1000,
+        oferta: true,
+        destacado: true,
+    },
+    {
+        id: "remera-02",
+        nombre: "Remera Copa - 'Messías'",
+        img: "remera-ronaldo.jpg",
+        categoria: {
+            nombre: "Remeras",
+            id: "remera"
+        },
+        precio: 1000,
+        oferta: true,
+        destacado: true,
+    },
+    {
+        id: "remera-03",
+        nombre: "Remera Copa - 'Diego ''82'",
+        img: "remera-ronaldo.jpg",
+        categoria: {
+            nombre: "Remeras",
+            id: "remera"
+        },
+        precio: 1000,
+        oferta: true,
+        destacado: true,
+    },
+    {
+        id: "remera-04",
+        nombre: "Remera Copa - 'Diego ''82'",
+        img: "remera-ronaldo.jpg",
+        categoria: {
+            nombre: "Remeras",
+            id: "remera"
+        },
+        precio: 1000,
+        oferta: true,
+        destacado: true,
+    }
+]
 
-    if (scrollPos > 40) {
-        navbar.style.top = "0"
-        navbar.style.transition = "0.5s ease-in-out";
-    }
-    else {
-        navbar.style.top = "40px"
-        navbar.style.transition = "0.5s ease-in-out";
-    }
-});  
+
+// SPLIDE JS
+const splideOffers = new Splide( '.splide-offers', {
+    perPage: '4',
+    perMove: '1',
+    gap: '20px',
+    breakpoints: {
+        992: {
+            perPage: 3,
+        },
+        576: {
+            perPage: 1,
+        }
+    },
+    pagination: false
+});
+
+const splideHero = new Splide( '.splide-hero', {
+    type: 'loop',
+    perPage: '1',
+    perMove: '1',
+    autoplay: true,
+    interval: 2500,
+    pauseOnHover: false,
+    arrows: false, 
+    type  : 'fade',
+    rewind: true,
+});
+
+const splideLocation = new Splide( '.splide-location', {
+    perPage: '1',
+    perMove: '1',
+    autoplay: true,
+    interval: 3000,
+    pauseOnHover: false,
+    type  : 'fade',
+    rewind: true,
+});
+
+
+
+
+splideHero.mount();
+splideLocation.mount();
+splideOffers.mount();
