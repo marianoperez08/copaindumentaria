3f1991f562a6639bc4c8958002d35c64b684ae7d
marianoperez.08 <marianoperez.c10@gmail.com>
2023-04-30 17:35:55 -0300

    changes desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,8 @@ let stockProductos = [
     {
         id: "remera-01",
         nombre: "Remera Copa - 'Fenómeno'",
-        img: "remera-ronaldo.jpg",
+        img1: "remera-ronaldo.jpg",
+        img2: "remera-ronaldo.jpg",
         categoria: {
             nombre: "Remeras",
             id: "remera"
@@ -49,6 +50,29 @@ let stockProductos = [
     }
 ]
 
+window.addEventListener("scroll", function() {
+    let navbar = document.querySelector(".navbar")
+    let img = document.querySelector(".navbar-img")
+    let navbarIcon = document.querySelector(".navbar-icon")
+    let cartCount = document.querySelector("#cart-count")
+    let scrollPos = window.scrollY
+
+    if (scrollPos > 1) {
+        navbar.style.top = "0"
+        navbar.style.backgroundColor = "var(--blue)"
+        navbar.style.transition = "background-color 0.3s ease-in-out";
+        img.src = "./assets/imgs/logos/logo-copa.png"
+        cartCount.style.backgroundColor = "var(--red)"
+    }
+    else {
+        navbar.style.top = "40px"
+        navbar.style.transition = "background-color 0.3s ease-in-out";
+        navbar.style.backgroundColor = "var(--red)"
+        img.src = "./assets/imgs/logos/logo-copa-white.png"
+        cartCount.style.backgroundColor = "var(--blue)"
+    }
+});  
+
 
 // SPLIDE JS
 const splideOffers = new Splide( '.splide-offers', {
@@ -56,8 +80,12 @@ const splideOffers = new Splide( '.splide-offers', {
     perMove: '1',
     gap: '20px',
     breakpoints: {
-        992: {
+        1100: {
             perPage: 3,
+        },
+
+        800: {
+            perPage: 2,
         },
         576: {
             perPage: 1,
